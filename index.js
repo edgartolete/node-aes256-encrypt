0ba44d4693cb9b4f52886cd89a32bd1c72a4ffb0
Edgar Tolete <edgartolete@outlook.com>
2023-08-05 21:49:54 +0800

    added hashing

--- index.js
+++ index.js
@@ -1,83 +1,96 @@
 
+const bcrypt = require('bcrypt');
 const crypto = require('crypto');
+const util = require('util');
 
-let cbcKey = null;
-let gcmKey = null;
-
-module.exports = {
-    cbc: {
-        setKey: (textKey) => cbcKey = Buffer.from(textKey, 'base64'),
-        encryptLong: (text) => cbcEncrypt(text),
-        decryptLong: (ivHex, encryptedText) => cbcDecrypt(ivHex, encryptedText),
-        encrypt: (data) => {
-            const {iv, text} = cbcEncrypt(data);
-            return iv + text;
-        },
-        decrypt: (data) => {
-            ivHex = data.slice(0,32);
-            encryptedText = data.slice(32)
-            return cbcDecrypt(ivHex, encryptedText)
+//returns only a hash: string;
+ const hash = (text) => {
+    const saltRounds = 10;
+    bcrypt.hash(text, saltRounds, (err, hash) =>{
+        if(err){
+            console.error('Error hashing password: ', err);
+        }else{
+            console.log('Hashed Password: ', hash);
+            return hash;
         }
-    },
-    gcm: {
-        setKey: (textKey) => gcmKey = Buffer.from(textKey, 'base64'),
-        encryptLong: (text) => gcmEncrypt(text),
-        decryptLong: (ivHex, encryptedText, tagHex) => gcmDecrypt(ivHex, encryptedText, tagHex),
-        encrypt: (data) =>{
-            const {iv, text, tag } = gcmEncrypt(data);
-            return iv + text + tag;
-        },
-        decrypt: (data) => {
-            const ivHex = data.slice(0, 24); 
-            const encryptedText = data.slice(24, -32); 
-            const tagHex = data.slice(-32); 
-            return gcmDecrypt(ivHex, encryptedText, tagHex);
-        },
-    },
-    keyGen: () => crypto.randomBytes(32).toString('base64')
+        return "";
+    });
+    return "";
 }
 
 
-const cbcEncrypt = (text) => {
-    const iv = crypto.randomBytes(16);
-    const cipher = crypto.createCipheriv("aes-256-cbc", cbcKey, iv);
-    let encrypted = cipher.update(text, 'utf8', 'hex');
-    encrypted += cipher.final('hex');
-    return {
-        iv: iv.toString('hex'),
-        text: encrypted
-    };
+//returns only boolean
+ const compare = (text, hash) => {
+    bcrypt.compare(text, hash, (err, result) =>{
+        if(err){
+            console.error('Error comparing password', err);
+        }else{
+            if(result === true){
+                console.log('Password is correct');
+                return true;
+            }else{
+                console.log('Password is incorrect')
+
+            }
+        }
+        return false;
+    })
+    return false;
 }
 
+const generatePBKDF2Key = (password) => {
+  const salt = crypto.randomBytes(16).toString('hex');
+  const iterations = 10000;
+  const keyLength = 32;
 
-const cbcDecrypt = (ivHex, encryptedText) =>{
-    const iv = Buffer.from(ivHex, 'hex');
-    const decipher = crypto.createDecipheriv("aes-256-cbc", cbcKey, iv);
-    let decrypted = decipher.update(encryptedText, 'hex', 'utf8');
-    decrypted += decipher.final('utf8');
-    return decrypted;
-}
+  return new Promise((resolve, reject) => {
+    crypto.pbkdf2(password, salt, iterations, keyLength, 'sha256', (err, derivedKey) => {
+      if (err) {
+        console.error(err);
+        reject(err);
+      } else {
+        resolve({ key: derivedKey.toString('hex'), salt: salt });
+      }
+    });
+  });
+};
+/**
+## Example usage:
+const password = 'mysecretpassword';
+generatePBKDF2Key(password)
+    .then((derivedKey) => {
+        console.log('PBKDF2 Key:', derivedKey);
+        return derivedKey;
+    })
+    .catch((err) => {
+        console.error('Error generating PBKDF2 key:', err);
+    });
+ */
 
 
-const gcmEncrypt = (text) => {
+const encrypt = (text, key) => {
     const iv = crypto.randomBytes(12); 
-    const cipher = crypto.createCipheriv("aes-256-gcm", gcmKey, iv);
+    const cipher = crypto.createCipheriv("aes-256-gcm", key, iv);
     const encrypted = Buffer.concat([cipher.update(text, 'utf8'), cipher.final()]);
     const tag = cipher.getAuthTag();
-    return {
-        iv: iv.toString('hex'),
-        text: encrypted.toString('hex'),
-        tag: tag.toString('hex')
-    };  
+    const result = iv.toString('hex') + encrypted.toString('hex') +tag.toString('hex');
+    return result;
 }
 
-const gcmDecrypt = (ivHex, encryptedText, tagHex) => {
+const decrypt = (text, key) => {
+    const ivHex = text.slice(0, 24); 
+    const encryptedText = text.slice(24, -32);
+    const tagHex = text.slice(-32); 
+
     const iv = Buffer.from(ivHex, 'hex');
     const encrypted = Buffer.from(encryptedText, 'hex');
     const tag = Buffer.from(tagHex, 'hex');
-    const decipher = crypto.createDecipheriv("aes-256-gcm", gcmKey, iv);
+    const decipher = crypto.createDecipheriv("aes-256-gcm", key, iv);
     decipher.setAuthTag(tag);
     let decrypted = decipher.update(encrypted, null, 'utf8');
     decrypted += decipher.final('utf8');
     return decrypted;
 }
+
+
+module.exports = {hash, compare, generatePBKDF2Key, encrypt, decrypt}
